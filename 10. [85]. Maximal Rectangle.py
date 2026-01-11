713c7aee06f6b39fe7aebf01dabbc79af1e0be07
Abdulla Al Noman <96348064+abdulla2025@users.noreply.github.com>
2026-01-11 14:21:23 +0600

    Add solution for problem 85: Maximal Rectangle

---
+++ 10. [85]. Maximal Rectangle.py
@@ -0,0 +1,38 @@
+class Solution:
+    def maximalRectangle(self, matrix: List[List[str]]) -> int:
+        if not matrix or not matrix[0]:
+            return 0
+        
+        rows, cols = len(matrix), len(matrix[0])
+        heights = [0] * cols
+        max_area = 0
+        
+        def largestRectangleArea(heights):
+            stack = []
+            max_area = 0
+            
+            for i, h in enumerate(heights):
+                while stack and heights[stack[-1]] > h:
+                    height_idx = stack.pop()
+                    height = heights[height_idx]
+                    width = i if not stack else i - stack[-1] - 1
+                    max_area = max(max_area, height * width)
+                stack.append(i)
+            
+            while stack:
+                height_idx = stack.pop()
+                height = heights[height_idx]
+                width = len(heights) if not stack else len(heights) - stack[-1] - 1
+                max_area = max(max_area, height * width)
+            
+            return max_area
+        for i in range(rows):
+            for j in range(cols):
+                if matrix[i][j] == '1':
+                    heights[j] += 1
+                else:
+                    heights[j] = 0
+            
+            max_area = max(max_area, largestRectangleArea(heights))
+        
+        return max_area
