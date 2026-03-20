d2f69f0e3ff43b3fc45e4a0de810ed00b3db1e38
Abdulla Al Noman <96348064+abdulla2025@users.noreply.github.com>
2026-03-20 23:57:55 +0600

    Implement minAbsDiff method for sliding submatrix

--- 75. 3567. Minimum Absolute Difference in Sliding Submatrix.py
+++ 75. 3567. Minimum Absolute Difference in Sliding Submatrix.py
@@ -1,1 +1,14 @@
-dd
+class Solution:
+    def minAbsDiff(self, grid: List[List[int]], k: int) -> List[List[int]]:
+        rows, cols = len(grid), len(grid[0])
+        ans = []
+        for top in range(rows - k + 1):
+            row = []
+            for left in range(cols - k + 1):
+                values = sorted({grid[top + dr][left + dc] for dr in range(k) for dc in range(k)})
+                if len(values) == 1:
+                    row.append(0)
+                else:
+                    row.append(min(values[i+1] - values[i] for i in range(len(values) - 1)))
+            ans.append(row)
+        return ans
