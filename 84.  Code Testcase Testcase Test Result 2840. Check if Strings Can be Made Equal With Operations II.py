cfd064f79fdde3c898c9c417925603cd3f925c92
Abdulla Al Noman <96348064+abdulla2025@users.noreply.github.com>
2026-03-30 20:56:44 +0600

    Implement string equality check with sorting

---
+++ 84.  Code Testcase Testcase Test Result 2840. Check if Strings Can be Made Equal With Operations II.py
@@ -0,0 +1,21 @@
+class Solution:
+    def checkStrings(self, s1: str, s2: str) -> bool:
+        even1 = []
+        even2 = []
+        odd1 = []
+        odd2 = []
+        for i in range(len(s1)):
+            if i % 2 == 0:
+                even1.append(s1[i])
+                even2.append(s2[i])
+            else:
+                odd1.append(s1[i])
+                odd2.append(s2[i])
+        even1.sort()
+        even2.sort()
+        odd1.sort()
+        odd2.sort()
+        if even1 == even2 and odd1 == odd2:
+            return True
+        else:
+            return False
