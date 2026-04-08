020452b91c3561759ff344d2f6fc00633d65977c
Abdulla Al Noman <96348064+abdulla2025@users.noreply.github.com>
2026-04-08 13:32:58 +0600

    Implement XOR calculation after applying range multiplication queries.

---
+++ 93. 3653. XOR After Range Multiplication Queries I.py
@@ -0,0 +1,13 @@
+class Solution:
+    def xorAfterQueries(self, nums: List[int], queries: List[List[int]]) -> int:
+        MOD = 10**9 + 7
+        
+        for l, r, k, v in queries:
+            for i in range(l, r + 1, k):
+                nums[i] = (nums[i] * v) % MOD
+        
+        result = 0
+        for num in nums:
+            result ^= num
+        
+        return result
