93203dd4847288ca2dab4f816e7bf09980ce4a6f
Abdulla Al Noman <96348064+abdulla2025@users.noreply.github.com>
2026-05-12 23:13:28 +0600

    Implement of Minimum Initial Energy to Finish Tasks
    Removed comments for clarity and streamlined code.

---
+++ 126. 1665. Minimum Initial Energy to Finish Tasks.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minimumEffort(self, tasks: List[List[int]]) -> int:
+        total_initial_energy = current_energy = 0
+
+        for actual_cost, min_required in sorted(tasks, key=lambda x: x[0] - x[1]):
+            if current_energy < min_required:
+                deficit = min_required - current_energy
+                total_initial_energy += deficit
+                current_energy = min_required
+            current_energy -= actual_cost
+        
+        return total_initial_energy
